1c0b28679d016238dc4aab5786a89d3e4c91f15b
ChihTai <ttootw@gmail.com>
2019-04-15 16:14:59 +0800

    0123456->1234567

--- calendar/index.php
+++ calendar/index.php
@@ -7,16 +7,19 @@ td{
   text-align:center;
   padding:10px;
 }
+.holiday{
+  background:pink;
+}
 </style>
 <table>
 <tr>
-  <td>星期日</td>
   <td>星期一</td>
   <td>星期二</td>
   <td>星期三</td>
   <td>星期四</td>
   <td>星期五</td>
   <td>星期六</td>
+  <td>星期日</td>
 </tr>
 <?php
 //紀念日陣列
@@ -30,39 +33,48 @@ $today=date("Y-m-d");
 $thisMonthFirstDay=date("Y-m-1");  //取得當月第一天字串
 //$thisMonthFirstDay="2019-09-01";
 //echo $thisMonthFirstDay;
-$theFirstDayWeek=date("w",strtotime($thisMonthFirstDay)); //取得第一天在第一周的星期
+if(date("w",strtotime($thisMonthFirstDay))==0){
+  $theFirstDayWeek=6; //取得第一天在第一周的星期
+}else{
+  $theFirstDayWeek=date("w",strtotime($thisMonthFirstDay))-1; //取得第一天在第一周的星期
+}
+
 $monthDays=date("t",strtotime($thisMonthFirstDay));  //取得當月的天數
-echo $thisMonthFirstDay ;
 
 
-
+echo "<br>";
 for($i=1;$i<=$monthDays;$i++){
   $date[]=date("Y-m-".sprintf("%02d",$i));
 }
 
-//print_r($date);
 $index=0;  //初始化索引值
 for($i=0;$i<6;$i++){
+  $holiday=""; //設定假日的class
   echo "<tr>";
+
   for($j=0;$j<7;$j++){
+    if($j>4){
+      $holiday="class='holiday'";
+    }
     $num=($i*7+$j); //計算格子數
+
     if($i==0 && $j<$theFirstDayWeek){
       
-      echo "<td></td>";
+      echo "<td $holiday></td>";
     }else if($num>($theFirstDayWeek+$monthDays-1)){  //判斷格子是否在當月的天數內
 
-      echo "<td></td>";
+      echo "<td $holiday></td>";
     }else{
 
       //根據畫到的天數來取出日期字串
       
       if(!empty($memo[$date[$index]])){
 
-        echo "<td>".$date[$index]."<br>".$memo[$date[$index]]."</td>";
+        echo "<td $holiday>".$date[$index]."<br>".$memo[$date[$index]]."</td>";
 
       }else{
 
-        echo "<td>".$date[$index]."</td>";
+        echo "<td $holiday>".$date[$index]."</td>";
       } 
       
       //echo "<td>".date("Y-m-d",strtotime("+$index days" . $thisMonthFirstDay))."</td>";
@@ -70,7 +82,6 @@ for($i=0;$i<6;$i++){
       $index++;
 
     }
-    
   }
   echo "</tr>";
 
@@ -78,7 +89,3 @@ for($i=0;$i<6;$i++){
 
 ?>
 </table>
-<?php
-
-
-?>
